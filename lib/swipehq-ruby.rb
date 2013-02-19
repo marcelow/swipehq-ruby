7f09241167d08da9803a3792a85e0a9637dd1dcc
Marcelo Wiermann <marcelo.wiermann@gmail.com>
2013-02-19 16:22:10 -0300

    start

---
+++ lib/swipehq-ruby.rb
@@ -0,0 +1,7 @@
+require "swipehq-ruby/version"
+
+module Swipehq
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/swipehq-ruby/version.rb
@@ -0,0 +1,5 @@
+module Swipehq
+  module Ruby
+    VERSION = "0.0.1"
+  end
+end
